f8526901dd7932c55601daadfbb185ff4c6f2f73
Nicolas Corona <njcorona@wharton.upenn.edu>
2021-02-02 14:17:19 -0500

    date column as datetime

--- analysis/main.py
+++ analysis/main.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 evite = pd.read_csv("/Users/saif/Downloads/data_cleaned_for_class.csv", index_col=0, parse_dates=True)
+evite["date"] = evite.apply(lambda x: datetime.strptime(x["date"][0:7], "%d%b%y"), axis=1)
 
 print(evite.columns.values.tolist())
 
