4074fa4f5de3cb2aeaf1243fff1e83d8f26b543d
Saif <68385788+safekhawaja@users.noreply.github.com>
2021-02-02 14:17:31 -0500

    ...

--- analysis/main.py
+++ analysis/main.py
@@ -16,7 +16,7 @@ columns = evite[["date", "events"]].to_numpy()
 x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
 y = np.array([5, 20, 14, 32, 22, 38])
 
-model = LinearRegression().fit(x, y)
+model = LinearRegression().fit(columns[0], columns[1])
 
 r_sq = model.score(x, y)
 
