5bc67d1dcf88ad56b88fe7e91bb9a158832f7323
Nicolas Corona <njcorona@wharton.upenn.edu>
2021-02-02 13:52:21 -0500

    R code
    Let's plan to do most of the analysis in Python - this is just for me messing around w the data.

---
+++ analysis/data_exploration.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+
+# Approaches
+
+## Time series
+## Linear regression
+## Logistic regression
+## Poisson regression / regression on some other distribution
+## Cluster analysis (clustering zip codes?)
+## Factor analysis (reducing number of coefficients)
+## Customer lifetime value: continuous, non-contractual
+### Although our data is discretized.
+
+
+orig_df <- read_csv("data_cleaned_for_class.csv")
+df <- orig_df
+
+###############
+# Cleaning data
+
+# Date
+df$date <- df$date %>% substr(1, 7) %>% as.Date("%d%b%y")
+
+# Standardizing for regression.  Uncomment the below when needed.
+# modelformula <- events ~ var1 + var2, where we want to standardize var1 and var2
+# standardized_df <- lapply(df[, all.vars(modelformula)], scale)
+
+df
+
