ad01ad96280aa06423482d73b0184cf0116e87fd
Saif <68385788+safekhawaja@users.noreply.github.com>
2021-02-02 14:05:58 -0500

    columns

--- analysis/main.py
+++ analysis/main.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 
 evite = pd.read_csv("/Users/saif/Downloads/data_cleaned_for_class.csv", index_col=0, parse_dates=True)
 
-evite.head()
+print(evite.columns.values.tolist())
 
-evite.plot()
+column_date = evite["date"].to_numpy()
+# evite.head()
 
-evite.plot.box()
+# evite.plot()
+# evite.plot.box()
 
 x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1))
 y = np.array([5, 20, 14, 32, 22, 38])
@@ -18,22 +20,22 @@ model = LinearRegression().fit(x, y)
 
 r_sq = model.score(x, y)
 
-print('coefficient of determination:', r_sq)
+# print('coefficient of determination:', r_sq)
 
-print('intercept:', model.intercept_)
-print('slope:', model.coef_)
+# print('intercept:', model.intercept_)
+# print('slope:', model.coef_)
 
 y_pred = model.intercept_ + model.coef_ * x
-print('predicted response:', y_pred, sep='\n')
+# print('predicted response:', y_pred, sep='\n')
 
-plt.plot(x, y, color='#444444', linestyle='--', label='')
+# plt.plot(x, y, color='#444444', linestyle='--', label='')
 
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Test Data')
 
-plt.savefig('plot.png')
-plt.show()
+# plt.savefig('plot.png')
+# plt.show()
 
 '''
 evite.shape
