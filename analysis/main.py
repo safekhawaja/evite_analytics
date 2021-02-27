d37acc95268178c3ebbe158332d9a2bfb7406a31
Saif <68385788+safekhawaja@users.noreply.github.com>
2021-02-26 23:33:33 -0500

    ...

--- analysis/main.py
+++ analysis/main.py
@@ -1,58 +1,83 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-# from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
 from datetime import datetime
 from sklearn.neighbors import KNeighborsRegressor
-# import statsmodels.api as sm
 from sklearn.metrics import r2_score
+from sklearn import svm
 
 # Importing Data
 
 evite = pd.read_csv("/Users/saif/Documents/GitHub/mktg401/data/before_covid.csv", index_col=0, parse_dates=True)
 evite["date"] = evite.apply(lambda x: datetime.strptime(x["date"][0:7], "%d%b%y"), axis=1)
+evite.date = evite.date.apply(pd.to_datetime)
 
-evite['index'] = evite.index
+evite['day'] = pd.DatetimeIndex(evite['date']).day
+evite['month'] = pd.DatetimeIndex(evite['date']).month
 
-# print(evite.date)
+dateday = list(zip(evite.day.values, evite.month.values))
+dates = np.array(dateday)
 
-'''
-fig = sm.qqplot(evite.events, line='45')
+
+def first(n):
+    return n[0]
+
+
+def sort(tuples):
+    return sorted(tuples, key=first)
+
+
+sorted_dates = sort(dateday)
+plt.scatter(sorted_dates[0], sorted_dates[1])
 plt.show()
+print(sorted_dates)
 
-Any vartiable of data indexed for time does not follow normal distribution but leaving this here to check
-'''
+# print(dateday)
+
+# dateday = [evite.date.day, evite.date.month]
+# evite.date.split()
+# print(dateday)
 
 # plt.plot(evite.date, evite.events, color='#444444', linestyle='--', label='')
 
-#plt.scatter(evite.date, evite.events, color='#444444', linestyle='--', label='')
+# plt.scatter(evite.date, evite.events, color='#444444', linestyle='--', label='')
 # plt.show()
 
 # Formatting Data
 
-columns_date = evite["date"].to_numpy()
-columns_events = evite["events"].to_numpy()
-col_ind = evite.index.to_numpy()
+# columns_date = evite["date"].to_numpy()
+# columns_events = evite["events"].to_numpy()
+# col_ind = evite.index.to_numpy()
 
-idd = col_ind.reshape(-1, 1)
-cd2d = columns_date.reshape(-1, 1)
-ce2d = columns_events.reshape(-1, 1)
+# idd = col_ind.reshape(-1, 1)
+# cd2d = columns_date.reshape(-1, 1)
+# ce2d = columns_events.reshape(-1, 1)
 
 # Making Predictions
 
-lin_reg = LinearRegression()
-model = lin_reg.fit(idd, ce2d)
-
-r_sq = model.score(idd, ce2d)
-
-print('coefficient of determination:', r_sq)
-
+# lin_reg = LinearRegression()
+# model = lin_reg.fit(idd, ce2d)
+#
+# r_sq = model.score(idd, ce2d)
+#
+# print('coefficient of determination:', r_sq)
+#
 # knn = KNeighborsRegressor(n_neighbors=3)
-# knn.fit(cd2d, ce2d)
-
-# print(knn.predict(cd2d[:3]))
+# knn.fit(idd, ce2d)
+#
+# # print(knn.predict(cd2d[:3]))
 # print(knn.score(cd2d, ce2d))
+
+
+# X = [[0, 0], [1, 1]]
+# y = [0, 1]
+# clf = svm.SVC()
+# clf.fit(X, y)
+# SVC()
+# clf.predict([[2., 2.]])
+
 
 '''
 # regression = linear_model.LinearRegression(degree=2) or:
@@ -68,7 +93,6 @@ print('coefficient of determination:', r_sq)
 # print('predicted response:', y_pred, sep='\n')
 
 # plt.plot(x, y, color='#444444', linestyle='--', label='')
-
 plt.xlabel('date')
 plt.ylabel('events')
 plt.title('Test Regression')
@@ -76,19 +100,20 @@ plt.title('Test Regression')
 # plt.savefig('plot.png')
 # plt.show()
 
-evite.shape
+Pandas commands:
 
+evite.shape
 evite.count()
 evite.columns
 evite.dtypes
-
-evite["name"] #returns name for every record
-
-evite["city"].unique() #returns the unique values in city
-
 evite['city'].value_counts() #counts the records for each city
-
 evite['category_0'].nunique() #counts the non-null unique values in category_0
+'''
 
-evite.groupby(['city']).agg([np.sum, np.mean, np.std])["stars"]
+'''
+import statsmodels.api as sm
+fig = sm.qqplot(evite.events, line='45')
+plt.show()
+
+Any vartiable of data indexed for time does not follow normal distribution but leaving this here to check
 '''
