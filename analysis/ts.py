dd683736c329d659723633b15ffe26c3f9938714
Saif <68385788+safekhawaja@users.noreply.github.com>
2021-03-01 15:30:49 -0500

    weekly days

--- analysis/ts.py
+++ analysis/ts.py
@@ -3,25 +3,51 @@ from datetime import datetime
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+# Rolling averages
+
 pre_evite = pd.read_csv("/Users/saif/Documents/GitHub/mktg401/data/before_covid.csv", index_col=0, parse_dates=True)
 pre_evite["date"] = pre_evite.apply(lambda x: datetime.strptime(x["date"][0:7], "%d%b%y"), axis=1)
 
-# pre_evite.rollingmean7 = pre_evite.events.rolling(window=7).mean()
-# pre_evite.rollingmean30 = pre_evite.events.rolling(window=30).mean()
-# pre_evite.rollingmean365 = pre_evite.events.rolling(window=365).mean()
-
 pre_evite_7 = pre_evite.assign(rm7=pre_evite.events.rolling(window=7).mean())
 pre_evite_30 = pre_evite_7.assign(rm30=pre_evite.events.rolling(window=30).mean())
-pre_evite_f = pre_evite_30.assign(rm365=pre_evite.events.rolling(window=365).mean())
+pre_evite_365 = pre_evite_30.assign(rm365=pre_evite.events.rolling(window=365).mean())
 
-print(pre_evite_f.rm7)
-print(pre_evite_f.rm30)
-print(pre_evite_f.rm365)
+print(pre_evite_365.rm7)
+print(pre_evite_365.rm30)
+print(pre_evite_365.rm365)
 
-mpl.rcParams['agg.path.chunksize'] = 10000
-plt.plot(pre_evite.date, pre_evite_f.rm7, color='#444444', linestyle='-', label='7-Day Moving Average')
-# plt.plot(pre_evite.date, pre_evite_f.rm30, color='#444444', linestyle='-', label='30-Day Moving Average')
-# plt.plot(pre_evite.date, pre_evite_f.rm365, color='#444444', linestyle='-', label='365-Day Moving Average')
-# plt.ylim(0, 2)
-plt.legend()
-plt.show()
+# Activity for days of the week
+
+mondays = []
+tuesdays = []
+wednesdays = []
+thursdays = []
+fridays = []
+saturdays = []
+sundays = []
+
+for row in pre_evite_365:
+    if pre_evite_365.loc[row, "date"].day_name() == "Monday":
+        mondays.append(pre_evite_365.loc[row, "events"])
+    elif pre_evite_365.loc[row, "date"].day_name() == "Tuesday":
+        tuesdays.append(pre_evite_365.loc[row, "events"])
+    elif pre_evite_365.loc[row, "date"].day_name() == "Wednesday":
+        wednesdays.append(pre_evite_365.loc[row, "events"])
+    elif pre_evite_365.loc[row, "date"].day_name() == "Thursday":
+        thursdays.append(pre_evite_365.loc[row, "events"])
+    elif pre_evite_365.loc[row, "date"].day_name() == "Friday":
+        fridays.append(pre_evite_365.loc[row, "events"])
+    elif pre_evite_365.loc[row, "date"].day_name() == "Saturday":
+        saturdays.append(pre_evite_365.loc[row, "events"])
+    else:
+        sundays.append(pre_evite_365.loc[row, "events"])
+
+weekdays = [mondays.mean(), tuesdays.mean(), wednesdays.mean(), thursdays.mean(), fridays.mean(), saturdays.mean(), sundays.mean()]
+
+print(weekdays)
+# mpl.rcParams['agg.path.chunksize'] = 10000
+# plt.plot(pre_evite.date, pre_evite_365.rm7, color='#444444', linestyle='-', label='7-Day Moving Average')
+# plt.plot(pre_evite.date, pre_evite_365.rm30, color='#444444', linestyle='--', label='30-Day Moving Average')
+# plt.plot(pre_evite.date, pre_evite_365.rm365, color='#444444', linestyle=':', label='365-Day Moving Average')
+# plt.legend()
+# plt.show()
