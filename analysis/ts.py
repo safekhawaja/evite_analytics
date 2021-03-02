4aacdf5aa0954d0022c75fb54b88201d2e9d229f
Saif <68385788+safekhawaja@users.noreply.github.com>
2021-03-01 19:46:40 -0500

    ..

--- analysis/ts.py
+++ analysis/ts.py
@@ -10,9 +10,9 @@ pre_evite["date"] = pre_evite.apply(lambda x: datetime.strptime(x["date"][0:7], 
 pre_evite.sort_values(by='date')
 pre_evite = pre_evite.groupby('date')['events'].sum().reset_index()
 
-pre_evite_7 = pre_evite.assign(rm7=pre_evite.events.rolling(window=7).mean())
-pre_evite_30 = pre_evite_7.assign(rm30=pre_evite.events.rolling(window=30).mean())
-pre_evite_365 = pre_evite_30.assign(rm365=pre_evite.events.rolling(window=365).mean())
+pre_evite_7 = pre_evite.assign(rm7=pre_evite.events.rolling(window=6).mean())
+pre_evite_30 = pre_evite_7.assign(rm30=pre_evite.events.rolling(window=3).mean())
+pre_evite_365 = pre_evite_30.assign(rm365=pre_evite.events.rolling(window=2).mean())
 
 mpl.rcParams['agg.path.chunksize'] = 10000
 plt.plot(pre_evite.date, pre_evite_365.rm7, color='#444444', linestyle='-', label='7-Day Moving Average')
