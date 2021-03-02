eab1ed2d3a007d54ea0f102a21c28c7f8c214d8e
Saif <68385788+safekhawaja@users.noreply.github.com>
2021-03-01 19:49:36 -0500

    whew

--- analysis/ts.py
+++ analysis/ts.py
@@ -15,7 +15,7 @@ pre_evite_30 = pre_evite_7.assign(rm30=pre_evite.events.rolling(window=3).mean()
 pre_evite_365 = pre_evite_30.assign(rm365=pre_evite.events.rolling(window=2).mean())
 
 mpl.rcParams['agg.path.chunksize'] = 10000
-plt.plot(pre_evite.date, pre_evite_365, color='#000000', linestyle='-', label='Total Events')
+plt.plot(pre_evite.date, pre_evite_365.events, color='#000000', linestyle='-', label='Total Events')
 plt.plot(pre_evite.date, pre_evite_365.rm7, color='#444444', linestyle='-', label='Bi-Monthly Moving Average')
 plt.plot(pre_evite.date, pre_evite_365.rm30, color='#444444', linestyle='--', label='Seasonal Moving Average')
 plt.plot(pre_evite.date, pre_evite_365.rm365, color='#444444', linestyle=':', label='Semi-Annual Moving Average')
