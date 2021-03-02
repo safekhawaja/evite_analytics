4f2408a478b9bedf5c7180e588be8256421a78a1
Saif <68385788+safekhawaja@users.noreply.github.com>
2021-03-02 14:38:51 -0500

    cleaned csv w/ knn

--- analysis/main.py
+++ analysis/main.py
@@ -3,82 +3,55 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
-from datetime import datetime
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import r2_score
-from sklearn import svm
 
 # Importing Data
 
-evite = pd.read_csv("/Users/saif/Documents/GitHub/mktg401/data/before_covid.csv", index_col=0, parse_dates=True)
-evite["date"] = evite.apply(lambda x: datetime.strptime(x["date"][0:7], "%d%b%y"), axis=1)
-evite.date = evite.date.apply(pd.to_datetime)
+evite = pd.read_csv("/Users/saif/Documents/GitHub/mktg401/analysis/rollingmeans.csv", index_col=0, parse_dates=True)
 
 # Formatting Data
 
-evite['day'] = pd.DatetimeIndex(evite['date']).day
-evite['month'] = pd.DatetimeIndex(evite['date']).month
-evite['year'] = pd.DatetimeIndex(evite['date']).year
+columns_date = evite["date"].to_numpy()
+columns_events = evite["events"].to_numpy()
+columns_index = evite.index.to_numpy()
 
-dateday = list(zip(evite.day.values, evite.month.values))
-dates = np.array(dateday)
-
-plt.scatter(evite.date, evite.events, color='#444444', linestyle='--', label='')
-plt.show()
-
-# columns_date = evite["date"].to_numpy()
-# columns_events = evite["events"].to_numpy()
-# col_ind = evite.index.to_numpy()
-
-# idd = col_ind.reshape(-1, 1)
-# cd2d = columns_date.reshape(-1, 1)
-# ce2d = columns_events.reshape(-1, 1)
+idd = columns_index.reshape(-1, 1)
+cd2d = columns_date.reshape(-1, 1)
+ce2d = columns_events.reshape(-1, 1)
 
 # Training Regression Models (linear, k-NN, logistic and polynomial regressors)
 
-# lin_reg = LinearRegression()
-# model = lin_reg.fit(idd, ce2d)
-#
-# r_sq = model.score(idd, ce2d)
-#
-# print('coefficient of determination:', r_sq)
-#
-# knn = KNeighborsRegressor(n_neighbors=3)
-# knn.fit(idd, ce2d)
-#
-# print(knn.predict(cd2d[:3]))
-# print(knn.score(cd2d, ce2d))
+lin_reg = LinearRegression()
+model = lin_reg.fit(idd, ce2d)
 
+r_sq = model.score(idd, ce2d)
 
-# X = [[0, 0], [1, 1]]
-# y = [0, 1]
-# clf = svm.SVC()
-# clf.fit(X, y)
-# SVC()
-# clf.predict([[2., 2.]])
+print('linear coefficient of determination:', r_sq)
 
+counter = 1
+
+while counter < 33:
+    knn = KNeighborsRegressor(n_neighbors=counter)
+    knn.fit(idd, ce2d)
+    print('k = ', counter, ' k-NN score:', knn.score(idd, ce2d))
+    counter = counter + 1
+
+poly_reg = PolynomialFeatures(degree=4)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly,y)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(X_poly,y)
+
+X_grid = np.arange(min(X),max(X),0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y,color='red')
+plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)),color='blue')
+plt.title('Truth or bluff(Polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
 
 '''
-# regression = linear_model.LinearRegression(degree=2) or:
-# poly = PolynomialFeatures(degree=2)
-# X_ = poly.fit_transform(X)
-# predict_ = poly.fit_transform(predict)
-
-
-# print('intercept:', model.intercept_)
-# print('slope:', model.coef_)
-
-# y_pred = model.intercept_ + model.coef_ * x
-# print('predicted response:', y_pred, sep='\n')
-
-# plt.plot(x, y, color='#444444', linestyle='--', label='')
-plt.xlabel('date')
-plt.ylabel('events')
-plt.title('Test Regression')
-
-# plt.savefig('plot.png')
-# plt.show()
-
 Pandas commands:
 
 evite.shape
@@ -94,5 +67,9 @@ import statsmodels.api as sm
 fig = sm.qqplot(evite.events, line='45')
 plt.show()
 
-Any vartiable of data indexed for time does not follow normal distribution but leaving this here to check
+Any variable of data indexed for time does not follow normal distribution but leaving this here to check
+
+# clf = svm.SVC()
+# clf.fit(idd, ce2d)
+# print('SVC coefficient of determination:', clf.score(idd, ce2d))
 '''
--- analysis/ts.py
+++ analysis/ts.py
@@ -3,21 +3,23 @@ from datetime import datetime
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-# Rolling averages
+# Rolling Averages
 
-pre_evite = pd.read_csv("/Users/saif/Downloads/data_cleaned_for_class.csv", index_col=0, parse_dates=True)
-pre_evite["date"] = pre_evite.apply(lambda x: datetime.strptime(x["date"][0:7], "%d%b%y"), axis=1)
-pre_evite.sort_values(by='date')
-pre_evite = pre_evite.groupby('date')['events'].sum().reset_index()
+df_evite = pd.read_csv("/Users/saif/Downloads/data_cleaned_for_class.csv", index_col=0, parse_dates=True)
+df_evite["date"] = df_evite.apply(lambda x: datetime.strptime(x["date"][0:7], "%d%b%y"), axis=1)
+df_evite.sort_values(by='date')
+df_evite = df_evite.groupby('date')['events'].sum().reset_index()
 
-pre_evite_7 = pre_evite.assign(rm7=pre_evite.events.rolling(window=6).mean())
-pre_evite_30 = pre_evite_7.assign(rm30=pre_evite.events.rolling(window=3).mean())
-pre_evite_365 = pre_evite_30.assign(rm365=pre_evite.events.rolling(window=2).mean())
+df_evite_6 = df_evite.assign(rm6=df_evite.events.rolling(window=6).mean())
+df_evite_3 = df_evite_6.assign(rm3=df_evite.events.rolling(window=3).mean())
+df_evite_f = df_evite_3.assign(rm2=df_evite.events.rolling(window=2).mean())
+
+df_evite_f.to_csv('rollingmeans.csv', index=True)
 
 mpl.rcParams['agg.path.chunksize'] = 10000
-plt.plot(pre_evite.date, pre_evite_365.events, color='#000000', linestyle='-', label='Total Events')
-plt.plot(pre_evite.date, pre_evite_365.rm7, color='#444444', linestyle='-', label='Bi-Monthly Moving Average')
-plt.plot(pre_evite.date, pre_evite_365.rm30, color='#444444', linestyle='--', label='Seasonal Moving Average')
-plt.plot(pre_evite.date, pre_evite_365.rm365, color='#444444', linestyle=':', label='Semi-Annual Moving Average')
+plt.plot(df_evite.date, df_evite_f.events, color='#000000', linestyle='-', label='Total Events')
+plt.plot(df_evite.date, df_evite_f.rm6, color='#444444', linestyle='-', label='Bi-Monthly Moving Mean')
+plt.plot(df_evite.date, df_evite_f.rm3, color='#444444', linestyle='--', label='Seasonal Moving Mean')
+plt.plot(df_evite.date, df_evite_f.rm2, color='#444444', linestyle=':', label='Semi-Annual Moving Mean')
 plt.legend()
 plt.show()
